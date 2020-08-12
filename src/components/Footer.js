360f4c74b91bface91bb6df9f6e8175d7fddca63
Ryan Kaplan <ryankaplan7@gmail.com>
2020-08-12 16:34:44 -0400

    flipped footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,16 @@ function Footer() {
         <Container fluid >
             <Row className="mt-5">
                 <Col md={4} />
-                <Col md={1} xs={6} className="githubIcon my-auto">
+                <Col md={1} xs={6} className="linkedinIcon my-auto">
+                    <Link
+                        to="linkedin">
+
+                        <div className="navBtn mx-4 text-center">
+                            <i className="icon fa fa-2x fa-linkedin my-3"></i>
+                        </div>
+                    </Link>
+                </Col>
+                <Col md={2} xs={6} className="githubIcon my-auto">
                     <Link
                         to="github">
 
@@ -17,15 +26,6 @@ function Footer() {
                         </div>
                     </Link>
                     {/* <MDBIcon fa icon="github" href="https://github.com/kapslock07" /> */}
-                </Col>
-                <Col md={2} xs={6} className="linkedinIcon my-auto">
-                    <Link
-                        to="linkedin">
-
-                        <div className="navBtn mx-4 text-center">
-                            <i className="icon fa fa-2x fa-linkedin my-3"></i>
-                        </div>
-                    </Link>
                 </Col>
                 <Col md={1}>
                     <div class="footer-copyright text-center my-3">© 2020
