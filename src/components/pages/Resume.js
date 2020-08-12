daca2afcb2ef56df65bd1b27d24024eadf7d18ea
Ryan Kaplan <ryankaplan7@gmail.com>
2020-08-12 17:00:55 -0400

    updated resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -15,7 +15,7 @@ function Resume() {
             <Row className="resumeBody mt-4 mx-auto">
                 <Col lg={12} className="mt-4 text-center">
                     {/* <embed src={ResumeDoc} className="z-depth-2" width="75%" height="1000" /> */}
-                    <iframe className="resume" src="https://docs.google.com/document/d/1--oCrJfuwaqlIYb1kMBmVvFPcUjacLGX3i_EM99hBwQ/preview" width="50%" height="1050" title="resume"></iframe>
+                    <iframe className="resume" src="https://drive.google.com/file/d/18_iDz0rK6N2yYneOL3V-aHSqRs2E5vqy/preview" width="50%" height="1050" title="resume"></iframe>
                     {/* <img src={ResumeDoc} className="img-fluid" alt="Resume"></img> */}
                 </Col>
             </Row>
