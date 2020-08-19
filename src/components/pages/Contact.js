c04d745ff4376cf70b564cd4b0b657aac6905f4e
Ryan Kaplan <ryankaplan7@gmail.com>
2020-08-18 22:39:22 -0400

    change icon sizing

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -17,25 +17,25 @@ function Contact() {
                     <div className="subHeader text-center">Ready to work together?</div>
                     <div className="contactRow">
                         <a href="mailto:ryankaplan7@gmail.com">
-                            <i className="fas fa-paper-plane fa-3x contactEmail"></i>
+                            <i className="fas fa-paper-plane contactEmail"></i>
                             <div className="contactEmail ml-3">ryankaplan7@gmail.com</div>
                         </a>
                     </div>
                     <div className="contactRow">
                         <a href="tel:631-662-2604">
-                            <i class="fas fa-phone fa-3x contactPhone"></i>
+                            <i class="fas fa-phone contactPhone"></i>
                             <div className="contactPhone ml-3">631-662-2604</div>
                         </a>
                     </div>
                     <div className="contactRow">
                         <a href="https://www.linkedin.com/in/ryankaplan7/">
-                            <i class="icon fa fa-3x fa-linkedin contactLinkedin"></i>
+                            <i class="icon fa fa-linkedin contactLinkedin"></i>
                             <div className="contactLinkedin ml-3">LinkedIn</div>
                         </a>
                     </div>
                     <div className="contactRow">
                         <a href="https://github.com/kapslock07">
-                            <i class="icon fa fa-3x fa-github contactGithub"></i>
+                            <i class="icon fa fa-github contactGithub"></i>
                             <div className="contactGithub ml-3">Github</div>
                         </a>
                     </div>
