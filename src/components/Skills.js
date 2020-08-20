57f735f1e6d3d7a05b6ff1d94e5a103fb3446b9d
Ryan Kaplan <ryankaplan7@gmail.com>
2020-08-20 13:16:30 -0400

    updated column spacing for skills on different mobile views

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,7 +5,7 @@ import { Col } from 'react-bootstrap';
 
 function Skills(props) {
     return (
-        <Col lg={3} xl={1} className="text-center mx-auto">
+        <Col lg={1} xl={1} className="skillRow text-center mx-auto">
             <div >
                 <img src={props.src} alt={props.name} width={props.dimension} height={props.dimension} className="skillIcon" >
                 </img>
